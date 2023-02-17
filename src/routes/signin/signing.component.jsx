44f015161baf7f67f0271d0e1b9867f08b0dee57
Bogdan Dobre <bogdandobre@Bogdans-MacBook-Pro.local>
2023-02-17 16:19:36 +0200

    created the sign-up component, the form input component and the button component for our sign-in page

--- src/routes/signin/signing.component.jsx
+++ src/routes/signin/signing.component.jsx
@@ -1,19 +1,39 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth, userDocRef } from '../../util/firebase/firebase.utils';
+// import { useEffect } from 'react';
+import { getRedirectResult } from 'firebase/auth';
+
+import { auth, signInWithGooglePopup,signInWithGoogleRedirect, createUserDocumentFromAuth, userDocRef } from '../../util/firebase/firebase.utils';
+
+import SignUpForm from '../../components/signup-form/signup-form.component';
+import Button from '../../components/button/button.component';
 
 const SignIn = () => {
 
+    // useEffect( () => {
+    // async function fetchData() {
+    //    const response = await getRedirectResult(auth);
+    //     if(response){
+    //         const userDocRef = await createUserDocumentFromAuth(response.user);
+    //     }
+    // }
+    // fetchData();
+    // }, []);
+    
     const logGoogleUser = async () => {
 
         const {user} = await signInWithGooglePopup();
-        createUserDocumentFromAuth(user);
+        const userDocRef = await createUserDocumentFromAuth(user);
     }
 
     return (
         <div>
         <h1> Sign In Page</h1>
-        <button onClick={logGoogleUser}>
+        <Button buttonType='google' onClick={logGoogleUser}>
         Sign In With Google Popup
-        </button>
+        </Button>
+        <SignUpForm />
+        {/*<button onClick={signInWithGoogleRedirect}>
+        Sign In With Google Redirect
+    </button>*/}
         </div>
     )
 }
